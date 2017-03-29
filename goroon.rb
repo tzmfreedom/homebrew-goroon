e0fc69f1a8991ed6044c59baa1b8a2fb79670c29
Makoto Tajitsu <makoto_tajitsu@hotmail.co.jp>
2017-03-30 00:09:14 +0900

    Update version 0.1.1

--- goroon.rb
+++ goroon.rb
@@ -2,15 +2,15 @@ class Goroon < Formula
   desc "Garoon Command Line Interface"
   homepage "https://github.com/tzmfreedom/goroon"
 
-  @@version = "0.1.0"
+  @@version = "0.1.1"
   version @@version
 
   if Hardware::CPU.is_64_bit?
     url "https://github.com/tzmfreedom/goroon/releases/download/v#{@@version}/goroon-#{@@version}-darwin-amd64.tar.gz"
-    sha256 'b0ce37342eaf7d0f0bc3c65f8d84160d72f055a6d8ce4b62a4c56a7945e55301'
+    sha256 'a5acf381c595ecbf144b40cfc178d7b2578b2c8f871221cc0d963a96e0490683'
   else
     url "https://github.com/tzmfreedom/goroon/releases/download/v#{@@version}/goroon-#{@@version}-darwin-386.tar.gz"
-    sha256 'a87215244d4dfc6f8866b683ecd85554a3fc5722483b8172213f63196f332138'
+    sha256 '231a2cfa97d98f87b8172cdf79df991becdb8750023b3316546196cb350a9848'
   end
 
   def install
