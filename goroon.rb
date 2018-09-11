e2d32b7ccdf58ea7d830a5d605cd4fdea5f47fcf
Makoto Tajitsu <makoto_tajitsu@hotmail.co.jp>
2018-09-11 20:10:48 +0900

    Update version 0.1.3

--- goroon.rb
+++ goroon.rb
@@ -2,17 +2,17 @@ class Goroon < Formula
   desc "Garoon Command Line Interface"
   homepage "https://github.com/tzmfreedom/goroon"
 
-  @@version = "0.1.2"
+  @@version = "0.1.3"
   version @@version
 
   option "without-completions", "Disable zsh completions"
 
   if Hardware::CPU.is_64_bit?
     url "https://github.com/tzmfreedom/goroon/releases/download/v#{@@version}/goroon-#{@@version}-darwin-amd64.tar.gz"
-    sha256 '9ee8c94092deae6cc3a0a94d8d50c40e5064d45f74badb41d61925835077e86a'
+    sha256 '497c4e4a0311ec0d7fcf0f96110b26f6196a4b8a327d73d70b609ec68a09ec08'
   else
     url "https://github.com/tzmfreedom/goroon/releases/download/v#{@@version}/goroon-#{@@version}-darwin-386.tar.gz"
-    sha256 '4fd36e7c3ceea41f53fab1e9bc03c3ec509756708d7d474e4f07c28778a246fc'
+    sha256 '9539012cc65929dd3ea2cd5de0c2d9d4d9e4315ce8f59e6408a0f0e465301229'
   end
 
   def install
